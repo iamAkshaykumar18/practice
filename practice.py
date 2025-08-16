dc4466549ea21b0b86de47eaf957d857cbaaddfb
Akshaykumar <iamakshaykumar18@gmail.com>
2025-08-16 17:15:07 +0000

    change

---
+++ practice.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the value of A: "))
+b = int(input("Enter the value of B: "))
+if a > b:
+    print("A is greater than B")
+elif a < b:
+    print("B is greater than A")
+else:
+    print("A and B are equal")
